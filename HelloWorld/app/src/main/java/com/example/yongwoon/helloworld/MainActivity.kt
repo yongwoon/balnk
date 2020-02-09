b3025b68eb0b5376214d558035dfb7aff8ecab98
yongwoon <yonyon.japan@gmail.com>
2020-02-09 21:41:24 +0900

    Regist button event process

--- HelloWorld/app/src/main/java/com/example/yongwoon/helloworld/MainActivity.kt
+++ HelloWorld/app/src/main/java/com/example/yongwoon/helloworld/MainActivity.kt
@@ -2,11 +2,31 @@ package com.example.yongwoon.helloworld
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+// morningBtn.setOnClickListener や greetingText.text などを関数として使うことができる
+import kotlinx.android.synthetic.main.activity_main.*
+import java.time.format.ResolverStyle
 
+// AppCompatActivity を継承します。
 class MainActivity : AppCompatActivity() {
 
+    // Kotlin の関数や method の宣言です。
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // 画面に表示する View をせっていします。
+        // ここで指定している R.layout.activity_main とういう定数は、
+        // activity_main.xml から生成された値です。
         setContentView(R.layout.activity_main)
+
+        morningBtn.setOnClickListener {
+            greetingText.text = getString(R.string.good_morning_text)
+        }
+
+        afternoonBtn.setOnClickListener {
+            greetingText.text = getString(R.string.afternoon_text)
+        }
+
+        eveningBtn.setOnClickListener {
+            greetingText.text = getString(R.string.evening_text)
+        }
     }
 }
