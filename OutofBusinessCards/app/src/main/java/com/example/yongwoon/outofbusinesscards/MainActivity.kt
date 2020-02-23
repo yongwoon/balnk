7a0723cc04cc6159301440a6cc122e1cd25d6d06
yongwoon <yonyon.japan@gmail.com>
2020-02-23 21:12:00 +0900

    Add View on main activity

--- OutofBusinessCards/app/src/main/java/com/example/yongwoon/outofbusinesscards/MainActivity.kt
+++ OutofBusinessCards/app/src/main/java/com/example/yongwoon/outofbusinesscards/MainActivity.kt
@@ -2,11 +2,40 @@ package com.example.yongwoon.outofbusinesscards
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
+import kotlinx.android.synthetic.main.activity_edit.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        PreferenceManager.getDefaultSharedPreferences(this).apply {
+            val company = getString("company", "")
+            val postal = getString("postal", "")
+            val address = getString("address", "")
+            val tel = getString("tel", "")
+            val fax = getString("fax", "")
+            val email = getString("email","")
+            val url = getString("url", "")
+            val position = getString("position", "")
+            val name = getString("name", "")
+
+            companyText.text = company
+            postalText.text = postal
+            addressText.text = address
+            telText.text = "tel:" + tel
+            faxText.text = "fax:" + fax
+            emailText.text = email
+            urlText.text = url
+            positionText.text = position
+            nameText.text = name
+        }
     }
 }
