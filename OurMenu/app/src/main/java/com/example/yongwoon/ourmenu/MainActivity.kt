1206bc2513d89910219e59e0ba997b7fa669e0fc
yongwoon <yonyon.japan@gmail.com>
2020-02-29 11:48:19 +0900

    Show menu in main activity

--- OurMenu/app/src/main/java/com/example/yongwoon/ourmenu/MainActivity.kt
+++ OurMenu/app/src/main/java/com/example/yongwoon/ourmenu/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.yongwoon.ourmenu
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
     }
 }
