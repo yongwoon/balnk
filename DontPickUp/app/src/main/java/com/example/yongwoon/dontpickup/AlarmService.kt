9ec2b62ffbb08eba474231243c44b3478a4d67a6
yongwoon <yonyon.japan@gmail.com>
2020-04-04 10:54:27 +0900

    Add alram service

---
+++ DontPickUp/app/src/main/java/com/example/yongwoon/dontpickup/AlarmService.kt
@@ -0,0 +1,12 @@
+package com.example.yongwoon.dontpickup
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class AlarmService : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}
