672764081145f113dbe0fb4977e86f3154529793
yongwoon <yonyon.japan@gmail.com>
2020-05-02 11:26:48 +0900

    set blackhole and geme process

--- Blackholes/app/src/main/java/com/example/yongwoon/blackholes/MainActivity.kt
+++ Blackholes/app/src/main/java/com/example/yongwoon/blackholes/MainActivity.kt
@@ -16,7 +16,10 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity(), SensorEventListener, SurfaceHolder.Callback {
     private val tag = "Black holes"
     private val matrixSize = 16
+    private val numOfBlackhole = 5 // 出現させる black home
     private val radius = 30f // ball の半径
+    private val limitOfBlackhole = 100 // この変数は blackhole があまり surface の端っこに出ないための値
+
     private var mgValues = FloatArray(3)
     private var acValues = FloatArray(3)
     private var startTime:Long = 0
@@ -25,6 +28,100 @@ class MainActivity : AppCompatActivity(), SensorEventListener, SurfaceHolder.Cal
     private var ballX = 0f
     private var ballY = 0f
     private var isGoal = false
+    private var isGone = false
+
+    private val blackholesList = ArrayList<Blackhole>()
+
+    override fun surfaceChanged(holder: SurfaceHolder?, format: Int, width: Int, height: Int) {
+        surfaceWidth = width
+        surfaceHeight = height
+        ballX = (width / 2).toFloat()
+        ballY = (height - radius).toFloat()
+        startTime = System.currentTimeMillis()
+        bornBlackholes()
+    }
+
+    private fun bornBlackholes() {
+        for( i in 1..numOfBlackhole) {
+            val x :Float = (limitOfBlackhole..surfaceWidth - limitOfBlackhole).random().toFloat()
+            val y :Float = (limitOfBlackhole..surfaceHeight - limitOfBlackhole).random().toFloat()
+            val spped :Int = (2..11).random()
+            val bh = Blackhole(x, y, spped)
+            blackholesList.add(bh)
+
+        }
+    }
+
+    private fun drawGameBoard(pitch: Int, roll :Int) {
+        ballX += roll
+        ballY -= pitch
+
+        // 左右は逆から
+        if (ballX < 0) {
+            ballX = surfaceWidth - radius
+        } else if (ballX > surfaceWidth) {
+            ballX = radius
+        }
+
+        // 上はゴール、下は落ちない
+        if (ballY + radius  < 0) {
+            isGoal = true
+        } else  if (ballY + radius > surfaceHeight) {
+            ballY = surfaceHeight - radius
+        }
+
+        // 飲み込まれたか
+        for(bh in blackholesList) {
+            if (checkGone(bh.x, bh.y, bh.r)) {
+                isGone = true
+            }
+        }
+
+        val canvas = surfaceView.holder.lockCanvas()
+        val paint = Paint()
+
+        canvas.drawColor(Color.BLUE)
+        paint.color = Color.BLACK
+        for(bh in blackholesList) {
+            canvas.drawCircle(bh.x, bh.y, bh.r, paint)
+            bh.grow()
+        }
+        paint.color = Color.YELLOW
+        if (!isGone ) {
+            canvas.drawCircle(ballX, ballY, radius, paint)
+        }
+        if (isGoal) {
+            paint.textSize = 80f
+            canvas.drawText(goaled(), 10f, (surfaceHeight - 60).toFloat(), paint)
+        }
+
+        surfaceView.holder.unlockCanvasAndPost(canvas)
+    }
+
+    private fun checkGone(x0: Float, y0: Float, r: Float): Boolean {
+        return (x0 - ballX) * (x0 - ballX) + (y0 - ballY) * (y0 - ballY) < r*r
+    }
+
+    private fun goaled(): String {
+        // 経過時間
+        val elapsedTime = System.currentTimeMillis() - startTime
+        val secTime = (elapsedTime / 1000).toInt()
+
+        return "Goal! ${secTime}秒"
+    }
+
+    override fun surfaceDestroyed(holder: SurfaceHolder?) {
+        val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sensorManager.unregisterListener(this)
+    }
+
+    override fun surfaceCreated(holder: SurfaceHolder?) {
+        var sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        val accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        val magField = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL)
+        sensorManager.registerListener(this, magField, SensorManager.SENSOR_DELAY_NORMAL)
+    }
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
     }
@@ -51,67 +148,13 @@ class MainActivity : AppCompatActivity(), SensorEventListener, SurfaceHolder.Cal
         Log.v(tag, "pitch${roll}")
         Log.v(tag, "roll${roll}")
 
-        if(!isGoal) {
+        if(!isGoal && !isGone) {
             drawGameBoard(pitch, roll)
         }
     }
 
     private fun rad2Deg(rad: Float): Int {
         return Math.floor(Math.toDegrees(rad.toDouble())).toInt()
-    }
-
-    override fun surfaceChanged(holder: SurfaceHolder?, format: Int, width: Int, height: Int) {
-        surfaceWidth = width
-        surfaceHeight = height
-        ballX = (width / 2).toFloat()
-        ballY = (height - radius).toFloat()
-        startTime = System.currentTimeMillis()
-    }
-
-    private fun drawGameBoard(pitch: Int, roll :Int) {
-        ballX += roll
-        ballY -= pitch
-
-        // 左右は逆から
-        if (ballX < 0) {
-            ballX = surfaceWidth - radius
-        } else if (ballX > surfaceWidth) {
-            ballY - surfaceHeight - radius
-        }
-
-        val canvas = surfaceView.holder.lockCanvas()
-        val paint = Paint()
-        paint.color = Color.YELLOW
-        canvas.drawColor(Color.BLUE)
-        canvas.drawCircle(ballX, ballY, radius, paint)
-
-        if (isGoal) {
-            paint.textSize = 80f
-            canvas.drawText(goaled(), 10f, (surfaceHeight - 60).toFloat(), paint)
-        }
-
-        surfaceView.holder.unlockCanvasAndPost(canvas)
-    }
-
-    private fun goaled(): String {
-        // 経過時間
-        val elapsedTime = System.currentTimeMillis() - startTime
-        val secTime = (elapsedTime / 1000).toInt()
-
-        return "Goal! ${secTime}秒"
-    }
-
-    override fun surfaceDestroyed(holder: SurfaceHolder?) {
-        val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        sensorManager.unregisterListener(this)
-    }
-
-    override fun surfaceCreated(holder: SurfaceHolder?) {
-        var sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        val accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
-        val magField = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)
-        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL)
-        sensorManager.registerListener(this, magField, SensorManager.SENSOR_DELAY_NORMAL)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -121,5 +164,4 @@ class MainActivity : AppCompatActivity(), SensorEventListener, SurfaceHolder.Cal
         val holder = surfaceView.holder
         holder.addCallback(this)
     }
-
 }
