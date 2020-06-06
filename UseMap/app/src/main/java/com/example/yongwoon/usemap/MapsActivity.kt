0b2e0076941ad33d259cc83631f9f5e93a1cf23e
yongwoon <yonyon.japan@gmail.com>
2020-06-06 10:30:20 +0900

    coding

--- UseMap/app/src/main/java/com/example/yongwoon/usemap/MapsActivity.kt
+++ UseMap/app/src/main/java/com/example/yongwoon/usemap/MapsActivity.kt
@@ -1,7 +1,12 @@
 package com.example.yongwoon.usemap
 
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -13,6 +18,7 @@ import com.google.android.gms.maps.model.MarkerOptions
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+    private val MY_PERMISSION_REQUEST_ACCESS_FINE_LOCATION = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,9 +41,64 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-//        val sydney = LatLng(-34.0, 151.0)
-//        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        checkPermission()
+    }
+
+    private fun checkPermission() {
+        // application が指定した permission を持っているかを判断します。
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            myLocationEnable()
+        } else {
+            requestLocationPermission()
+        }
+   }
+
+    private fun requestLocationPermission() {
+        // shouldShowRequestPermissionRationale は、user が Permission を名刺的に拒否したかどうかを返します。
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)){
+            // 許可を求め、拒否されていた場合
+            // 許可 dialog を表示して、user に許可を求めます。
+            // 許可して欲しい permission を配列に指定して、複数の permission を一度に求めることができます。
+            // また、許可されたのがどの permission の要求か知るために request code も引数に渡します。
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), MY_PERMISSION_REQUEST_ACCESS_FINE_LOCATION)
+        } else {
+            // まだ魚価を求めていない
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), MY_PERMISSION_REQUEST_ACCESS_FINE_LOCATION)
+        }
+    }
+
+    // onRequestPermissionsResult は、許可 dialog に対して user が選択した結果を受け取ります。
+    // 許可 dialog で permission が許可、あるいは拒否されたときに処理を行うには、onRequestPermissionsResult を override します。
+    // 第 1 引数: requestPermissions() の第３引数で指定した request code が渡ってきます。
+    // 第 2 引数: 指定した permission が配列で渡ってきます。
+    // 第 3 引数: 第 2 引数の配列と pair になる形で結果が渡ってきます。
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode) {
+            MY_PERMISSION_REQUEST_ACCESS_FINE_LOCATION-> {
+                if(permissions.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // 許可された
+                    myLocationEnable()
+                } else {
+                    showToast("現在位置は表示できません。")
+                }
+            }
+        }
+    }
+
+    private fun myLocationEnable() {
+        // 赤い波線でエラーが表示されてしまうので書く
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            mMap.isMyLocationEnabled = true
+        }
+    }
+
+    private fun showToast(msg: String) {
+        val toast = Toast.makeText(this, msg, Toast.LENGTH_LONG)
+        toast.show()
     }
 }
