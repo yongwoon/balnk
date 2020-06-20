f07f1f0d92d25808ecf2bf92f6a7128a3083b40c
yongwoon <yonyon.japan@gmail.com>
2020-06-20 11:24:13 +0900

    現在位置の軽度・緯度の取得(FusedLocationProviderClient)

--- MemoMemoTime/app/src/main/java/com/example/yongwoon/memomemotime/MainActivity.kt
+++ MemoMemoTime/app/src/main/java/com/example/yongwoon/memomemotime/MainActivity.kt
@@ -2,18 +2,29 @@ package com.example.yongwoon.memomemotime
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.*
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var mMap: GoogleMap
     private val MY_PERMISSION_REQUEST_ACCESS_FINE_LOCATION = 1
+
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    // 位置情報を受け取るための Location object
+    private lateinit var lastLocation: Location
+    private var locationCallback: LocationCallback? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +34,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
@@ -72,11 +84,43 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         // 赤い波線でエラーが表示されてしまうので
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             mMap.isMyLocationEnabled = true
+
+            val locationRequest = LocationRequest().apply {
+                interval = 10000 // 最も長い更新時間(ms)
+                fastestInterval = 5000 // 最も短い更新時間(ms)
+                priority = LocationRequest.PRIORITY_HIGH_ACCURACY // 最も高精度
+            }
+
+            // onLocationResult() は、位置情報を取得しっときに呼び出される method です。位置情報を lastLocation に取得し、
+            // LatLng 容にして currentLatLng に代入します。それを map の moveCamera() method に引数に指定することで、
+            // map を動かします。また、TextView に 軽度・緯度 を表示します。
+            // locationCallback を与えて実行することで、realtime に位置情報を取得できるようになりま。
+            locationCallback = object : LocationCallback() {
+                override fun onLocationResult(locationResult: LocationResult?) {
+                    if(locationResult?.lastLocation != null) {
+                        lastLocation = locationResult.lastLocation
+                        val currentLatLng = LatLng(lastLocation.latitude, lastLocation.longitude)
+
+                        mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLatLng))
+                        textView.text = "Lat:${lastLocation.latitude}, Lng:${lastLocation.longitude}"
+                    }
+                }
+            }
+
+            fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null)
         }
     }
 
     private fun showToast(msg: String) {
         val toast = Toast.makeText(this, msg, Toast.LENGTH_LONG)
         toast.show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        if(locationCallback != null) {
+            fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+        }
     }
 }
